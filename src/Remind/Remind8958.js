7093ec897d394b9d187be29674a29f389df13324
Kukjin Lee <75208878+cptkuk91@users.noreply.github.com>
2021-09-25 00:44:40 +0900

    BaekJoon Node Until No.4344

--- src/Remind/Remind8958.js
+++ src/Remind/Remind8958.js
@@ -1,23 +1,19 @@
 let fs = require('fs');
-
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let number = Number(input[0]);
+let numbers = Number(input[0]);
 
-for (let i = 1; i <= number; i++) {
+for (let i = 1; i <= numbers; i++) {
     let count = 0;
     let sum = 0;
+
     for (let j = 0; j < input[i].length; j++) {
         if (input[i][j] === 'O') {
             count++;
-        // count + count = 2? why 3?
-        // why 중단 후 처음부터 다시 count + 1?
         } else {
             count = 0;
         }
         sum += count;
-        // sum = count + sum
-        // sum = count + sum
     }
     console.log(sum);
 }

774176fed7c63a9e1dd43126d3212656c72e0155
Kukjin Lee <75208878+cptkuk91@users.noreply.github.com>
2021-10-03 23:49:41 +0900

    Update Velog.io/cptkuk91

--- src/Test.js
+++ src/Test.js
@@ -1,14 +1,5 @@
-function checkAge(age){
-    if(age >= 18){
-        return true;
-    } else {
-        return console.log('보호자 동의가 필요합니다.');
-    }
-}
-
-let age = 18;
-if(checkAge(age)){
-    console.log('접속 허용');
-} else{
-    console.log('접속 차단');
+let user = {
+    name: null,
 };
+
+let admin = user;

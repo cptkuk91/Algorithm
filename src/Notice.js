8a74e6946440b396499b73d1dddc0ecb10b0837a
cptkuk91 <cptkuk91@gmail.com>
2022-05-19 22:50:32 +0900

    [Programmers] 추가

--- src/Notice.js
+++
@@ -1,9 +0,0 @@
-let s = 10
-for(let i = 0; i < s; i++){
-    console.log(s);
-}
-
-/*
-velog.io/cptkuk91
-글 많이 작성했습니다.
- */
--- src/Test11654.js
+++
@@ -1,7 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-let word = input[0];
-let result = word.charCodeAt(0);
-
-console.log(result);

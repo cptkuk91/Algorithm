5c9cf30f43c72406fb4b5a4fbe6196c1abae4b14
cptkuk91 <cptkuk91@gmail.com>
2022-06-15 15:12:41 +0900

    두 정수 사이의 합

---
+++ Programmers/12912.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+  let answer = 0;
+  // return answer;
+  if (a <= b) {
+    for (let i = a; i <= b; i++) {
+      answer += i;
+    }
+  } else {
+    for (let j = b; j <= a; j++) {
+      answer += j;
+    }
+  }
+  return answer;
+}

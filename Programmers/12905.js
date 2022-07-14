34fbdf5a17c22568e595ab785d91bbb55b412bac
cptkuk91 <cptkuk91@gmail.com>
2022-07-14 22:57:48 +0900

    가장 큰 정사각형

--- Programmers/12905.js
+++ Programmers/12905.js
@@ -1,7 +1,23 @@
 function solution(board) {
-  let answer = 1234;
-  // 풀이법을 모르겠다..
-  console.log("Hello World");
+  let answer = 0;
+  let row = board.length;
+  let column = board[0].length;
 
-  return answer;
+  if (row <= 1 || column <= 1) {
+    return 1;
+  }
+
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < column; j++) {
+      if (board[i][j] > 0) {
+        let up = board[i - 1][j];
+        let left = board[i][j - 1];
+        let cross = board[i - 1][j - 1];
+        let minNum = Math.min(up, left, cross);
+        board[i][j] = minNum + 1;
+        answer = Math.max(answer, board[i][j]);
+      }
+    }
+  }
+  return answer * answer;
 }

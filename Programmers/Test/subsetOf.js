cb1e97910b3919a3554d16aa697d7f01b953a7c0
cptkuk91 <cptkuk91@gmail.com>
2022-08-06 11:43:04 +0900

    부분집합 확인하기

--- Programmers/Test/subsetOf.js
+++ Programmers/Test/subsetOf.js
@@ -1,1 +1,25 @@
 // subsetOf는 부분집합을 뜻한다.
+function solution(base, sample) {
+  base.sort((a, b) => a - b);
+  sample.sort((a, b) => a - b);
+
+  const checkArr = (find, compare, from) => {
+    for (let i = from; i < compare.length; i++) {
+      if (find === compare[i]) {
+        return i;
+      } else if (find < compare[i]) {
+        return -1;
+      }
+    }
+    return -1;
+  };
+
+  let flag = 0;
+  for (let i = 0; i < sample.length; i++) {
+    flag = checkArr(sample[i], base, flag);
+    if (flag === -1) {
+      return false;
+    }
+  }
+  return true;
+}

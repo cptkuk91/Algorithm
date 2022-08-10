3209891acdd4c43636ccaaf8da507a49cb76d7df
cptkuk91 <cptkuk91@gmail.com>
2022-08-11 02:09:27 +0900

    indexOf를 활용한 decrypt

--- Programmers/Test/decrypt.js
+++ Programmers/Test/decrypt.js
@@ -1,3 +1,15 @@
-function solution(str, char) {
-  console.log("Hello World");
+function solution(str, secret) {
+  let result = "";
+  let alpha = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      result += " ";
+    } else {
+      let current = alpha.indexOf(str[i]);
+      let after = (current - secret + alpha.length) % alpha.length;
+      result += alpha[after];
+    }
+  }
+  return result;
 }

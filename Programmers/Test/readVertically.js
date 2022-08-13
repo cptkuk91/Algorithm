cdc4c203b67c78a7e07c8e22bbec782fafcdf717
cptkuk91 <cptkuk91@gmail.com>
2022-08-13 21:59:14 +0900

    배열과 반복문을 통한 문자 섞기

--- Programmers/Test/readVertically.js
+++ Programmers/Test/readVertically.js
@@ -1,5 +1,22 @@
 function solution(arr) {
   let result = "";
 
+  let temp = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let str = arr[i];
+    for (let j = 0; j < str.length; j++) {
+      if (temp.length === j) {
+        temp.push(str[j]);
+      } else {
+        temp[j] += str[j];
+      }
+    }
+  }
+
+  for (let k = 0; k < temp.length; k++) {
+    result += temp[k];
+  }
+
   return result;
 }

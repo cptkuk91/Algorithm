32f6a3bf9693fcf2df19ad27ed77fb955adaa2f2
cptkuk91 <cptkuk91@gmail.com>
2022-08-14 23:29:40 +0900

    가장 긴 문자열과 짧은 문자열 제외하고 배열에 push 하기

--- Programmers/Test/removeExtremes.js
+++ Programmers/Test/removeExtremes.js
@@ -1,5 +1,28 @@
 function solution(arr) {
   let result = [];
 
+  let longestLen = 0;
+  let longestIdx = 0;
+  let shortestLen = 20; // 주의 사항에 가장 긴 경우는 20이하를 갖는다고 한다. 0을 설정할 경우 공식 성립 x
+  let shortestIdx = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length >= longestLen) {
+      longestLen = arr[i].length;
+      longestIdx = i;
+    }
+
+    if (arr[i].length <= shortestLen) {
+      shortestLen = arr[i].length;
+      shortestIdx = i;
+    }
+  }
+
+  for (let j = 0; j < arr.length; j++) {
+    if (j !== longestIdx && j !== shortestIdx) {
+      result.push(arr[j]);
+    }
+  }
+
   return result;
 }

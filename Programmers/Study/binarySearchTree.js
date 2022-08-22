c433bdca927e1c7efb76ca2342e44ce6a47e004a
cptkuk91 <cptkuk91@gmail.com>
2022-08-23 04:51:44 +0900

    binarySearchTree 구현

---
+++ Programmers/Study/binarySearchTree.js
@@ -0,0 +1,72 @@
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+    } else {
+      return false;
+    }
+  }
+
+  contains(value) {
+    if (this.value === value) {
+      return true;
+    }
+    if (value < this.value) {
+      return !!(this.left && this.left.contains(value));
+    }
+
+    if (value > this.value) {
+      return !!(this.right && this.right.contains(value));
+    }
+    return false;
+  }
+
+  // 전위 순회
+  preorder(callback) {
+    callback(this.value);
+    if (this.left) {
+      this.left.preorder(callback);
+    }
+    if (this.right) {
+      this.right.preorder(callback);
+    }
+  }
+
+  // 중위 순회
+  inorder(callback) {
+    if (this.left) {
+      this.left.inorder(callback);
+    }
+    callback(this.value);
+    if (this.right) {
+      this.right.inorder(callback);
+    }
+  }
+
+  // 후 순회
+  postorder(callback) {
+    if (this.left) {
+      this.left.postorder(callback);
+    }
+    if (this.right) {
+      this.right.postorder(callback);
+    }
+    callback(this.value);
+  }
+}

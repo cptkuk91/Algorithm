37f80458c3c3f9eebc112f54e4dd03ae43688a7d
cptkuk91 <cptkuk91@gmail.com>
2022-10-12 01:34:49 +0900

    프로그래머스 0단계 해결 중

---
+++ Programmers/KAKAO/120803.js
@@ -0,0 +1,5 @@
+function solution(num1, num2) {
+  let result = 0;
+  result = num1 - num2;
+  return result;
+}
---
+++ Programmers/KAKAO/120820.js
@@ -0,0 +1,5 @@
+function solution(age) {
+  let fixed = 2022;
+  result = fixed - age + 1;
+  return result;
+}

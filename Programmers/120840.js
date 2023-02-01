0e4524d56e6cfe3435d8519c3e0c09c9a1683888
cptkuk91 <cptkuk91@gmail.com>
2023-02-01 12:04:21 +0900

    [추가] 구슬을 나누는 경우의 수

---
+++ Programmers/120840.js
@@ -0,0 +1,26 @@
+// function solution(balls, share) {
+// 	return factorial(balls) / (factorial(balls - share) * factorial(share));
+// }
+
+// function factorial(number){
+// 	let basic = BigInt(1);
+
+//     for(let i = 2; i <= number; i++){
+//     	basic *= BigInt(i);
+//     }
+//     return basic;
+// }
+
+function solution(balls, share) {
+  return factorial(balls) / (factorial(balls - share) - factorial(share));
+}
+
+function factorial(number) {
+  let basic = BigInt(1);
+
+  for (let i = 2; i <= number; i++) {
+    basic *= BigInt(i);
+  }
+
+  return basic;
+}

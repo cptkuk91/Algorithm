c85fcdbe8fbb33beeb5fb35b7cbf8320deb1d16b
cptkuk91 <cptkuk91@gmail.com>
2023-03-17 23:44:26 +0900

    [추가] 실패율

--- Programmers/42889.js
+++ Programmers/42889.js
@@ -9,5 +9,12 @@ function solution(N, stages) {
     tmpNum -= tmp;
   }
 
+  result = result.sort((a, b) => b[1] - a[1]);
+  result = result.map((el) => el[0]);
+
+  // 반드시 result에 할당해야합니다.
+  // sort한 결과를 담아야 map에서 정렬된 result를 사용할 수 있고,
+  // map은 새로운 배열을 반환하기 때문에 반드시 할당해줘야 합니다.
+
   return result;
 }

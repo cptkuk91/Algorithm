c36718425f339efddcbf3181fe6a6c065d7bfb0b
cptkuk91 <cptkuk91@gmail.com>
2023-03-25 20:57:54 +0900

    [추가] 특정 값 배열에서 제거하기

---
+++ leetcode/remove_element.js
@@ -0,0 +1,17 @@
+// 현재 배열을 유지하고, 새로운 배열 생성 없이
+// 특정 값 제거하기
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
